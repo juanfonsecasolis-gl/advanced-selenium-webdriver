fd723d781c905f9aca313ca05a7315027118934c
Juan Fonseca <juanma2268@gmail.com>
2021-10-03 18:19:10 -0600

    Updated with Linux uux Webdriver

--- PART 4/Lecture 33/advanced-selenium-webdriver/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
+++ PART 4/Lecture 33/advanced-selenium-webdriver/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
@@ -22,7 +22,7 @@ public class BrowserDriverFactory {
 
 		switch (browser) {
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			driver.set(new ChromeDriver());
 			break;
 
@@ -33,7 +33,7 @@ public class BrowserDriverFactory {
 
 		default:
 			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			driver.set(new ChromeDriver());
 			break;
 		}
